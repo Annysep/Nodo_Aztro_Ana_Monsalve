769b8f94ff667d12c4e95171f0033291ad312c9d
Ana Maria <sep911_anny@hotmail.com>
2024-06-28 15:51:57 -0500

    Termino reto

---
+++ Reto5 Logica secuencial/logica.js
@@ -0,0 +1,37 @@
+// Rectangulo
+let altura  = 0
+let base = 0
+
+let area = 0
+let perimetro = 0
+let diagonal = 0
+
+area = base * altura
+perimetro = 2 * base + 2 * altura
+diagonal = Math.sqrt(base * base + altura * altura)
+
+console.log(area); 
+console.log(perimetro); 
+console.log(diagonal); 
+
+// Circulo
+
+const Pi = 3.14159;
+let diametro = 10
+let radio = 0
+
+let area2 = 0
+let perimetro2 = 0
+
+radio = diametro / 2
+
+area2 = Pi * radio * radio
+perimetro2 = 2 * Pi * radio
+
+console.log(radio)
+console.log(area2)
+console.log(perimetro2)
+
+
+
+
